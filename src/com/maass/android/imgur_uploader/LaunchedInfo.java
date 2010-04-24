b0d4ce79f0ba5cd78fe67055e8cfa2b875913ab1
ALok <alok0@reddit>
2010-04-24 03:01:23 -0400

    Add new launchable Activity so that the noobs have somewhere to go to find "instructions"

---
+++ src/com/maass/android/imgur_uploader/LaunchedInfo.java
@@ -0,0 +1,15 @@
+package com.maass.android.imgur_uploader;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class LaunchedInfo extends Activity {
+	/**
+	 * @see android.app.Activity#onCreate(Bundle)
+	 */
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.info);
+	}
+}
