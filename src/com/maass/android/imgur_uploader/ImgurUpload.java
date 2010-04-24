157a51564be4693345c60a08c198c353989b7d2c
ALok <alok0@reddit>
2010-04-24 01:34:52 -0400

    remove unused import

--- src/com/maass/android/imgur_uploader/ImgurUpload.java
+++ src/com/maass/android/imgur_uploader/ImgurUpload.java
@@ -44,7 +44,6 @@ import android.text.ClipboardManager;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
