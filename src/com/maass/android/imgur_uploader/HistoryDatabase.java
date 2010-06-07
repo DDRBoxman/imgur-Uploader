bcca8260ab764660b5b582f367f20108aec682ef
Colin Edwards <ddrboxman@gmail.com>
2010-06-06 22:02:34 -0400

    Tweaked Preferences activity Put framework into HistoryDatabase to facilitate auto clean, just needs queries

--- src/com/maass/android/imgur_uploader/HistoryDatabase.java
+++ src/com/maass/android/imgur_uploader/HistoryDatabase.java
@@ -1,8 +1,10 @@
 package com.maass.android.imgur_uploader;
 
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.preference.PreferenceManager;
 import android.util.Log;
 
 public class HistoryDatabase extends SQLiteOpenHelper {
@@ -10,6 +12,8 @@ public class HistoryDatabase extends SQLiteOpenHelper {
     private static final String DATABASE_NAME = "imgur.db";
     private static final int DATABASE_VERSION = 11;
     private static final String IMGUR_TABLE_NAME = "imgur_history";
+
+    private Context context;
 
     // id is necessary so cursor adapter can iterate over table
     private static final String IMGUR_TABLE_CREATE = "CREATE TABLE "
@@ -21,6 +25,27 @@ public class HistoryDatabase extends SQLiteOpenHelper {
     HistoryDatabase(final Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
         Log.i(this.getClass().getName(), "in cstr");
+
+        this.context = context;
+    }
+
+    @Override
+    public void onOpen(final SQLiteDatabase db) {
+        final SharedPreferences prefs = PreferenceManager
+            .getDefaultSharedPreferences(context);
+
+        //delete if too many images
+        if (prefs.getBoolean("clean_max_images", false)) {
+            final int maxImages = prefs.getInt("clean_max_images_number", -1);
+            //            db.query("imgur_history", new String[] { "_idx" }, null, null,
+            //                null, null, "_idx");
+        }
+
+        //delete if image is too old
+        if (prefs.getBoolean("clean_old_images", false)) {
+            final int maxAge = prefs.getInt("clean_old_images_date", -1);
+        }
+
     }
 
     @Override
